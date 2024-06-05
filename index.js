dcee45b84ff780458634a57736ed9b68929ff816
FEROZ KHAN <Feroz3151@gmail.com>
2024-06-05 11:10:54 +0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+import 'dotenv/config';
+// require('dotenv').config();
+
 import express from "express";
+import mongoose from 'mongoose';
+import cors from "cors";
+// import routes from "./Routes/Index.js";
+
 
 
 const app = express();
 const PORT = 5000;
+const DB_URL = process.env.DATABASE_URL;
+console.log(DB_URL); 
+
+
+
+mongoose.connect(DB_URL)
+mongoose.connection.on("connected", () => console.log("mongose connect"));
+mongoose.connection.on("error", (err) => console.log("error mongo", err));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 
 
 
