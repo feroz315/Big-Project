02be20cc7cd4b317530d80619d4b98ed5ab03bdd
FEROZ KHAN <Feroz3151@gmail.com>
2024-06-20 20:01:00 +0500

    update

--- Routes/Index.js
+++ Routes/Index.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 import { getAllProducts,getSingleProduct } from "../Controller/Product.js";
 import { UserSignup,UserLogin,UserLogOut } from "../Controller/Auth.js";
 import { authtoken } from "../Middlewares/Index.js";
+import { products } from "../Products.js";
 
 
 
@@ -13,7 +14,7 @@ routes.get("/products", getAllProducts,authtoken);
 routes.get("/products/:id", async (req, res) => {
     const { id } = req.params;
     try {
-      const singleproduct = await userData.findById({ _id: id });
+      const singleproduct = await products.findById({ _id: id });
       res.status(200).json(singleproduct);
     } catch (error) {
       res.status(500).json({ error: error.message });
